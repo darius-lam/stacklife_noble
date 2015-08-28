67e0414b91c83f23e9a32a957eb524f3ea54892c
Cameron Wong <cam@localhost.localdomain>
2015-08-28 11:52:15 +0000

    Make search?advanced=true breaking cloud.php

--- src/web/translators/cloud.php
+++ src/web/translators/cloud.php
@@ -1,13 +1,13 @@
 <?php
-    
+
   require_once (__DIR__ .  '/../../../etc/sl_ini.php');
-  
+
   //gets the id "string" of the book.  Find more in .htaccess
   $q = $_GET['query'];
 
   $q = urlencode($q);
   $offset = $_GET['start'];
-  $limit = $_GET['limit']; 
+  $limit = $_GET['limit'];
   $search_type = $_GET['search_type'];
   $sort = urlencode($_GET['sort']);
 
@@ -26,7 +26,7 @@
           $url = $url . "&facet=" . $value;
     }
   }
-  
+
   $filter_list = array();
   $filter_string = '';
     foreach (explode('&', $incoming) as $pair) {
@@ -42,23 +42,23 @@
   $contents = fetch_page($url);
 
   $xml = simplexml_load_string($contents, "SimpleXMLElement", LIBXML_NOCDATA);
-  $j = json_encode($xml); 
+  $j = json_encode($xml);
 
   $book_data = json_decode($j);
 
-  
+
   $hits = $book_data->totalResults;
-  
+
   $items = $book_data->mods;
-    
+
   $books_fields = array('id', 'title','creator','measurement_page_numeric','measurement_height_numeric', 'shelfrank', 'pub_date', 'title_link_friendly', 'format', 'loc_call_num_sort_order', 'link');
-    
+
   foreach($items as $item) {
     $title = '';
     $author = '';
-      
+
     $shelfrank = 35;
-      
+
 
     if(is_array($item->name)){
         foreach ($item->name as $name){
@@ -71,8 +71,8 @@
             $creator = array($item->name->namePart);
         }
     }
-      
-      
+
+
     if (!empty($item->titleInfo->title)) {
         $title =  preg_replace("/[^A-Za-z0-9_\s-]/", "",$item->titleInfo->title);
 
@@ -83,7 +83,7 @@
             $title = ($item->titleInfo[0]->nonSort) . $title;
         }
     }
-    
+
     $title_nf = $title;
     $title_link_friendly = strtolower($title_nf);
     //Make alphanumeric (removes all other characters)
@@ -94,79 +94,84 @@
     $title_link_friendly = preg_replace("/\s+$/", "", $title_link_friendly);
     //Convert whitespaces and underscore to dash
     $title_link_friendly = preg_replace("/[\s_]/", "-", $title_link_friendly);
-      
+
     if (!empty($item->physicalDescription->extent)) {
-        
+
         if( preg_match('/([1-9]*\s*)(?=cm)/',$item->physicalDescription->extent,$height) ){
             $height_cm = $height[0];
         }
         if( preg_match('/([1-9]*\s*)(?=p)/',$item->physicalDescription->extent,$p)) {
             $pages = $p[0];
         }
-        
+
     }
-      
+
     if(!$height_cm || $height_cm > 33 || $height_cm < 20) $height_cm = 27;
     if(!$pages) $pages = 200;
-      
+
     if(is_array($item->originInfo->dateIssued)){
         $year = intval($item->originInfo->dateIssued[1]);
     }else{
         $year = intval($item->originInfo->dateIssued);
     }
-    
-   
+
+
     $format = $item->typeOfResource;
     //need to fix
     if($format == "text"){
         $format = "Book";
     }
-      
+
     $year = substr($year, 0, 4);
     //$format = str_replace(" ", "", $format);
-    
+
     //still don't have sort order
     //$loc_sort_order = $item->loc_call_num_sort_order'];
     $loc_sort_order= 10;
-      
+
     $push = true;
-    if(!empty($item->identifier[0]->{'@attributes'}->invalid) && ($item->identifier[0]->{'@attributes'}->invalid == 'yes')){
-        //$link = "/item/" . $title_link_friendly . '/' . $item->recordInfo->recordIdentifier;  
+    $itemid = $item->identifier;
+    if (is_array($item->identifier)) {
+      $itemid = $item->identifier[0];
+    }
+    // I love inconsistent data!
+    if(!empty($itemid->{'@attributes'}->invalid) && ($itemid->{'@attributes'}->invalid == 'yes')){
+        //$link = "/item/" . $title_link_friendly . '/' . $item->recordInfo->recordIdentifier;
         //$id = 000;
-        
+
         $hits = $hits - 1;
         $push = false;
         //we simply don't push this item if it doesn't have a valid ISBN.
     }else{
         //may run into errors with this regex.
-        if((!is_string($item->identifier[0]))){
+        if((!is_string($itemid))){
             $push = false;
             $hits = $hits - 1;
         }else{
-            $isbn = preg_replace("/\s.*/","",$item->identifier[0]);
+            $isbn = preg_replace("/\s.*/","",$itemid);
             $id = $isbn;
             $link = $www_root . "/item/" . $title_link_friendly . '/' . $isbn;
         }
     }
-    
-      
+
+
     $books_data   = array($id, $title, $creator, $pages, $height_cm, $shelfrank, $year, $title_link_friendly, $format, $loc_sort_order, $link);
     $temp_array  = array_combine($books_fields, $books_data);
     if($push){
-       array_push($json, $temp_array); 
+       array_push($json, $temp_array);
     }
   }
-    
+
   $last = $offset + 10;
-    
+
     header('Content-type: application/json');
 
   if(count($json) == 0 || $offset == -1) {
-    echo '{"start": "-1", "num_found": ' . $hits . ', "limit": "0", "docs": ""}'; 
+    echo '{"start": "-1", "num_found": ' . $hits . ', "limit": "0", "docs": ""}';
   }
   else {
-    //echo '{"start": ' . $last. ', "limit": "' . $limit . '", "num_found": ' . $hits . ', "docs": ' . json_encode($json) . ', "facets": ' . json_encode($facets) . '}'; 
-      echo '{"start": ' . $last. ', "limit": "' . $limit . '", "num_found": ' . $hits . ', "docs": ' . json_encode($json) . '}'; 
+    //echo '{"start": ' . $last. ', "limit": "' . $limit . '", "num_found": ' . $hits . ', "docs": ' . json_encode($json) . ', "facets": ' . json_encode($facets) . '}';
+      echo '{"start": ' . $last. ', "limit": "' . $limit . '", "num_found": ' . $hits . ', "docs": ' . json_encode($json) . '}';
   }
 
 function fetch_page($url) {
@@ -178,9 +183,9 @@ function fetch_page($url) {
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 	$contents = curl_exec ($ch);
-	
+
 	curl_close ($ch);
-	
+
 	return $contents;
 }
 ?>
