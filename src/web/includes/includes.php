a6ceaa47d25d5affa08a16e1101dae384ecf00ac
Darius Lam <dariuslamikins@gmail.com>
2015-09-22 16:38:46 -0400

    minor changes

--- src/web/includes/includes.php
+++ src/web/includes/includes.php
@@ -26,6 +26,19 @@ echo <<<EOF
   <link rel="stylesheet" href="$www_root/css/template.css" type="text/css" />
   <link rel="stylesheet" href="$www_root/stackview/jquery.stackview.css" type="text/css" />
 
+
+    <script type="text/javascript">
+    if (document.location.hostname.search("noblenet.org") !== -1) {
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-52252320-5', 'auto');
+          ga('send', 'pageview');
+    }
+    </script>
+
   <!--[if IE]>
         <link rel="stylesheet" href="$www_root/stackview/ie.stackview.css" type="text/css" />
         <link rel="stylesheet" href="$www_root/css/ie.template.css" type="text/css" />
