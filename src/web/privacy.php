ce7f2dad258cb107eed3be199f69f6ce5a0312c8
Darius Lam <dariuslamikins@gmail.com>
2015-10-22 19:12:54 -0400

    minor webpage title changes

--- src/web/privacy.php
+++ src/web/privacy.php
@@ -6,7 +6,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>StackLife</title>
+<title>StackLife @ NOBLE</title>
 
 <?php
 include_once('includes/includes.php');
